7569e21750bffaa89669eb4b806b4e2b4f58c745
Page Brooks <pagebrooks@gmail.com>
2011-12-08 07:49:21 -0500

    Adding some inflection hueristics to the PluralizedAutoClassMapper

---
+++ DapperExtensions.Test/PluralizedAutoClassMapperFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DapperExtensions.Test.Data;
+using NUnit.Framework;
+
+namespace DapperExtensions.Test
+{
+    [TestFixture]
+    public class PluralizedAutoClassMapperFixture : BaseFixture
+    {
+        [Test]
+        public void TableName_Should_Properly_Pluralize()
+        {
+            PluralizedAutoClassMapper<Foo> m = new PluralizedAutoClassMapper<Foo>();
+            m.Table("robot");
+            Assert.AreEqual("robots", m.TableName);
+        }
+
+        [Test]
+        public void TableName_Should_Properly_Pluralize_Words_Ending_With_Y()
+        {
+            PluralizedAutoClassMapper<Foo> m = new PluralizedAutoClassMapper<Foo>();
+            m.Table("penny");
+            Assert.AreEqual("pennies", m.TableName);
+        }
+
+        [Test]
+        public void TableName_Should_Properly_Pluralize_Words_Ending_With_S()
+        {
+            PluralizedAutoClassMapper<Foo> m = new PluralizedAutoClassMapper<Foo>();
+            m.Table("mess");
+            Assert.AreEqual("messes", m.TableName);            
+        }
+
+        public class Foo
+        {
+        }
+    }
+}
--- DapperExtensions/ClassMapper.cs
+++ DapperExtensions/ClassMapper.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace DapperExtensions
 {
@@ -97,11 +98,60 @@ namespace DapperExtensions
         }
     }
 
-    public class PlurizedAutoClassMapper<T> : AutoClassMapper<T> where T : class
+    public class PluralizedAutoClassMapper<T> : AutoClassMapper<T> where T : class
     {
         public override void Table(string tableName)
         {
-            base.Table(tableName + "s");
+            base.Table(Formatting.Pluralize(2, tableName));
+        }
+    }
+
+    // http://mattgrande.wordpress.com/2009/10/28/pluralization-helper-for-c/
+    public class Formatting
+    {
+        private static readonly IList<string> Unpluralizables = new List<string> { "equipment", "information", "rice", "money", "species", "series", "fish", "sheep", "deer" };
+        private static readonly IDictionary<string, string> Pluralizations = new Dictionary<string, string>
+        {
+            // Start with the rarest cases, and move to the most common
+            { "person", "people" },
+            { "ox", "oxen" },
+            { "child", "children" },
+            { "foot", "feet" },
+            { "tooth", "teeth" },
+            { "goose", "geese" },
+            // And now the more standard rules.
+            { "(.*)fe?", "$1ves" },         // ie, wolf, wife
+            { "(.*)man$", "$1men" },
+            { "(.+[aeiou]y)$", "$1s" },
+            { "(.+[^aeiou])y$", "$1ies" },
+            { "(.+z)$", "$1zes" },
+            { "([m|l])ouse$", "$1ice" },
+            { "(.+)(e|i)x$", @"$1ices"},    // ie, Matrix, Index
+            { "(octop|vir)us$", "$1i"},
+            { "(.+(s|x|sh|ch))$", @"$1es"},
+            { "(.+)", @"$1s" }
+        };
+
+        public static string Pluralize(int count, string singular)
+        {
+            if (count == 1)
+                return singular;
+
+            if (Unpluralizables.Contains(singular))
+                return singular;
+
+            var plural = "";
+
+            foreach (var pluralization in Pluralizations)
+            {
+                if (Regex.IsMatch(singular, pluralization.Key))
+                {
+                    plural = Regex.Replace(singular, pluralization.Key, pluralization.Value);
+                    break;
+                }
+            }
+
+            return plural;
         }
     }
 }
