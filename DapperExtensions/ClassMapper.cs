0bf627501d7e3321be4df57f964ec278dbcd1e3f
Page Brooks <pagebrooks@gmail.com>
2011-12-13 08:47:37 -0500

    Minor syntax adjustments to PluralizedAutoClassMapper

--- DapperExtensions/ClassMapper.cs
+++ DapperExtensions/ClassMapper.cs
@@ -103,44 +103,41 @@ namespace DapperExtensions
     {
         public override void Table(string tableName)
         {
-            base.Table(Formatting.Pluralize(2, tableName));
+            base.Table(Formatting.Pluralize(tableName));
         }
         
-        // http://mattgrande.wordpress.com/2009/10/28/pluralization-helper-for-c/
+        // Adapted from: http://mattgrande.wordpress.com/2009/10/28/pluralization-helper-for-c/
         private static class Formatting
         {
             private static readonly IList<string> Unpluralizables = new List<string> { "equipment", "information", "rice", "money", "species", "series", "fish", "sheep", "deer" };
             private static readonly IDictionary<string, string> Pluralizations = new Dictionary<string, string>
-        {
-            // Start with the rarest cases, and move to the most common
-            { "person", "people" },
-            { "ox", "oxen" },
-            { "child", "children" },
-            { "foot", "feet" },
-            { "tooth", "teeth" },
-            { "goose", "geese" },
-            // And now the more standard rules.
-            { "(.*)fe?", "$1ves" },         // ie, wolf, wife
-            { "(.*)man$", "$1men" },
-            { "(.+[aeiou]y)$", "$1s" },
-            { "(.+[^aeiou])y$", "$1ies" },
-            { "(.+z)$", "$1zes" },
-            { "([m|l])ouse$", "$1ice" },
-            { "(.+)(e|i)x$", @"$1ices"},    // ie, Matrix, Index
-            { "(octop|vir)us$", "$1i"},
-            { "(.+(s|x|sh|ch))$", @"$1es"},
-            { "(.+)", @"$1s" }
-        };
-
-            public static string Pluralize(int count, string singular)
             {
-                if (count == 1)
-                    return singular;
+                // Start with the rarest cases, and move to the most common
+                { "person", "people" },
+                { "ox", "oxen" },
+                { "child", "children" },
+                { "foot", "feet" },
+                { "tooth", "teeth" },
+                { "goose", "geese" },
+                // And now the more standard rules.
+                { "(.*)fe?", "$1ves" },         // ie, wolf, wife
+                { "(.*)man$", "$1men" },
+                { "(.+[aeiou]y)$", "$1s" },
+                { "(.+[^aeiou])y$", "$1ies" },
+                { "(.+z)$", "$1zes" },
+                { "([m|l])ouse$", "$1ice" },
+                { "(.+)(e|i)x$", @"$1ices"},    // ie, Matrix, Index
+                { "(octop|vir)us$", "$1i"},
+                { "(.+(s|x|sh|ch))$", @"$1es"},
+                { "(.+)", @"$1s" }
+            };
 
+            public static string Pluralize(string singular)
+            {
                 if (Unpluralizables.Contains(singular))
                     return singular;
 
-                var plural = "";
+                var plural = string.Empty;
 
                 foreach (var pluralization in Pluralizations)
                 {
