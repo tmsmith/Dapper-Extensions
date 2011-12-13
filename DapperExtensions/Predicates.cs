5c59ec255cc593dac4e8597223ed4a57066445b6
Page Brooks <pagebrooks@gmail.com>
2011-12-13 10:12:24 -0500

    Adding API documentation

--- DapperExtensions/Predicates.cs
+++ DapperExtensions/Predicates.cs
@@ -10,6 +10,16 @@ namespace DapperExtensions
 {
     public static class Predicates
     {
+        /// <summary>
+        /// Factory method that creates a new IFieldPredicate predicate: [FieldName] [Operator] [Value]. 
+        /// Example: WHERE FirstName = 'Foo'
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="expression">An expression that returns the left operand [FieldName].</param>
+        /// <param name="op">The comparison operator.</param>
+        /// <param name="value">The value for the predicate.</param>
+        /// <param name="not">Effectively inverts the comparison operator. Example: WHERE FirstName &lt;&gt; 'Foo'.</param>
+        /// <returns>An instance of IFieldPredicate.</returns>
         public static IFieldPredicate Field<T>(Expression<Func<T, object>> expression, Operator op, object value, bool not = false) where T : class
         {
             PropertyInfo propertyInfo = ReflectionHelper.GetProperty(expression) as PropertyInfo;
@@ -22,6 +32,17 @@ namespace DapperExtensions
                        };
         }
 
+        /// <summary>
+        /// Factory method that creates a new IPropertyPredicate predicate: [FieldName1] [Operator] [FieldName2]
+        /// Example: WHERE FirstName = LastName
+        /// </summary>
+        /// <typeparam name="T">The type of the entity for the left operand.</typeparam>
+        /// <typeparam name="T2">The type of the entity for the right operand.</typeparam>
+        /// <param name="expression">An expression that returns the left operand [FieldName1].</param>
+        /// <param name="op">The comparison operator.</param>
+        /// <param name="expression2">An expression that returns the right operand [FieldName2].</param>
+        /// <param name="not">Effectively inverts the comparison operator. Example: WHERE FirstName &lt;&gt; LastName </param>
+        /// <returns>An instance of IPropertyPredicate.</returns>
         public static IPropertyPredicate Property<T, T2>(Expression<Func<T, object>> expression, Operator op, Expression<Func<T2, object>> expression2, bool not = false) 
             where T : class
             where T2 : class
@@ -37,6 +58,13 @@ namespace DapperExtensions
                        };
         }
 
+        /// <summary>
+        /// Factory method that creates a new IPredicateGroup predicate.
+        /// Predicate groups can be joined together with other predicate groups.
+        /// </summary>
+        /// <param name="op">The grouping operator to use when joining the predicates (AND / OR).</param>
+        /// <param name="predicate">A list of predicates to group.</param>
+        /// <returns>An instance of IPredicateGroup.</returns>
         public static IPredicateGroup Group(GroupOperator op, params IPredicate[] predicate)
         {
             return new PredicateGroup
@@ -46,6 +74,9 @@ namespace DapperExtensions
                        };
         }
 
+        /// <summary>
+        /// Factory method that creates a new IExistsPredicate predicate.
+        /// </summary>
         public static IExistsPredicate Exists<TSub>(IPredicate predicate, bool not = false)
             where TSub : class
         {
@@ -56,6 +87,9 @@ namespace DapperExtensions
                        };
         }
 
+        /// <summary>
+        /// Factory method that creates a new Sort which controls how the results will be sorted.
+        /// </summary>
         public static ISort Sort<T>(Expression<Func<T, object>> expression, bool ascending = true)
         {
             PropertyInfo propertyInfo = ReflectionHelper.GetProperty(expression) as PropertyInfo;
