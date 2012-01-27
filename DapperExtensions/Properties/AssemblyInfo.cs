9aa5a0a7f7b0d98dd87c2bbf2ce94b0f21956cfe
Page Brooks <pagebrooks@gmail.com>
2012-01-27 12:36:21 -0500

    Adding assembly-level CLSCompliant attribute

--- DapperExtensions/Properties/AssemblyInfo.cs
+++ DapperExtensions/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -37,3 +38,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: AssemblyInformationalVersion("1.3.0-a")]
+
+[assembly: CLSCompliant(true)]
