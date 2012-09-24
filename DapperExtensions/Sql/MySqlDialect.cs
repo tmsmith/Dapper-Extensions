c3d83ca284e7ddcddd47afcd41264f5c5d7117c4
Thad Smith <thadmsmith@gmail.com>
2012-09-24 15:12:49 -0400

    marked as obsolete as tests failed using dialect.

--- DapperExtensions/Sql/MySqlDialect.cs
+++ DapperExtensions/Sql/MySqlDialect.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 namespace DapperExtensions.Sql
 {
+    [Obsolete("Not ready from primetime - use at your own risk", false)]
     public class MySqlDialect : SqlDialectBase
     {
         public override char OpenQuote
