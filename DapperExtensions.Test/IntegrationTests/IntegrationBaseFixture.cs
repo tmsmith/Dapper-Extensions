25f699ff28e742ed968b129759d6b31257048e41
Thad Smith <thadmsmith@gmail.com>
2012-09-24 15:04:43 -0400

    changes to mysql sqlscripts

--- DapperExtensions.Test/IntegrationTests/IntegrationBaseFixture.cs
+++ DapperExtensions.Test/IntegrationTests/IntegrationBaseFixture.cs
@@ -23,7 +23,7 @@ namespace DapperExtensions.Test.IntegrationTests
         {
             Databases.Add(GetSqlConnection("Data Source=.;Initial Catalog=dapperTest;Integrated security=True;"));
             Databases.Add(GetSqlCeConnection("Data Source=.\\dapperTest.sdf"));
-            //Databases.Add(GetMySqlConnection("Server=localhost;Port=3306;Database=dapperTest;uid=root;password=acsdev123!"));
+            //Databases.Add(GetMySqlConnection("Server=localhost;Port=3306;Database=dapperTest;uid=root;password=password!"));
 
             foreach (var database in Databases)
             {
