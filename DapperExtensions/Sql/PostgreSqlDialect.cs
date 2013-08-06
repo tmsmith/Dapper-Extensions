1748512067cad62af143a4bf3a46baca76d639eb
tmsmith <thadmsmith@gmail.com>
2013-08-06 18:17:24 -0400

    added postgresql dialect. upgraded to latest dapper

---
+++ DapperExtensions/Sql/PostgreSqlDialect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DapperExtensions.Sql
+{
+    public class PostgreSqlDialect : SqlDialectBase
+    {
+        public override string GetIdentitySql(string tableName)
+        {
+            return "SELECT LASTVAL() AS Id";
+        }
+
+        public override string GetPagingSql(string sql, int page, int resultsPerPage, IDictionary<string, object> parameters)
+        {
+            string result = string.Format("{0} LIMIT @resultsPerPage OFFSET @pageStartRowNbr", sql);
+            int startValue = page * resultsPerPage;
+            parameters.Add("@pageStartRowNbr", startValue);
+            parameters.Add("@resultsPerPage", resultsPerPage);
+            return result;
+        }
+
+        public override string GetColumnName(string prefix, string columnName, string alias)
+        {
+            return base.GetColumnName(null, columnName, alias).ToLower();
+        }
+
+        public override string GetTableName(string schemaName, string tableName, string alias)
+        {
+            return base.GetTableName(schemaName, tableName, alias).ToLower();
+        }
+    }
+
+}
