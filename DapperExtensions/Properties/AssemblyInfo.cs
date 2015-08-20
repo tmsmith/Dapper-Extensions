b64c3c01264db81e58657d00e275e2c36853cd79
Lukas Tines <ltines@euroffice.co.uk>
2015-08-20 10:22:07 +0100

    Package 2.2

--- DapperExtensions/Properties/AssemblyInfo.cs
+++ DapperExtensions/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DapperExtensions")]
-[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,8 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("792b61ca-432b-4db2-a5b3-d2fb5043d0ed")]
-
+[assembly: Guid("BA43EF7C-0EB4-4BA6-8130-A7CAD17BB08D")]
 [assembly: InternalsVisibleTo("DapperExtensions.Test")]
 [assembly: InternalsVisibleTo("DapperExtensions.Test.SQLite")]
 
@@ -36,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.4.2")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0.0")]
 
 [assembly: CLSCompliant(true)]
