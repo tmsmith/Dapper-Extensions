4804562b328cc966e0217a557151a6742df2a59d
Ryan Watson <ryan.watson@live.co.za>
2015-10-29 08:38:12 +0300

    configure nuget spec

--- Dapper.Extensions.Linq/Properties/AssemblyInfo.cs
+++ Dapper.Extensions.Linq/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dapper.Extensions.Linq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small library that complements Dapper by adding basic CRUD operations (Get, Insert, Update, Delete) for your POCOs. For more advanced querying scenarios, Dapper.Extensions.Linq allows the use of Linq.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dapper.Extensions.Linq")]
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.3")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
 [assembly: CLSCompliant(true)]
