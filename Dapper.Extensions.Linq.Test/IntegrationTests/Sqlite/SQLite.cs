0e07a1c9885c9885f91ad4368597a93eb8e77b6a
Ryan Watson <ryan.watson@live.co.za>
2015-11-11 12:51:23 +0200

    SQLite unit tests

---
+++ Dapper.Extensions.Linq.Test/IntegrationTests/Sqlite/SQLite.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Dapper.Extensions.Linq.CastleWindsor;
+using Dapper.Extensions.Linq.Core.Configuration;
+using Dapper.Extensions.Linq.Mapper;
+using Dapper.Extensions.Linq.SQLite;
+using Dapper.Extensions.Linq.Test.IntegrationTests.Fixtures;
+using NUnit.Framework;
+
+namespace Dapper.Extensions.Linq.Test.IntegrationTests.SQLite
+{
+    public class SQLite : FixturesBase
+    {
+        private SQLiteConnection _connection;
+
+        [TestFixtureSetUp]
+        public void RunBeforeAnyTests()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["__DefaultSQLite"].ConnectionString;
+            string[] connectionParts = connectionString.Split(';');
+            string file = connectionParts
+                .ToDictionary(k => k.Split('=')[0], v => v.Split('=')[1])
+                .Where(d => d.Key.Equals("Data Source", StringComparison.OrdinalIgnoreCase))
+                .Select(k => k.Value).Single();
+
+            if (File.Exists(file))
+                File.Delete(file);
+
+            Container = new Castle.Windsor.WindsorContainer();
+
+            DapperConfiguration
+                .Use()
+                .UseClassMapper(typeof(AutoClassMapper<>))
+                .UseContainer<WindsorContainer>(cfg => cfg.UseExisting(Container))
+                .UseSqlDialect(new SQLiteDialect())
+                .WithDefaultConnectionStringNamed("__DefaultSQLite")
+                .FromAssembly("Dapper.Extensions.Linq.Test.Entities")
+                .FromAssembly("Dapper.Extensions.Linq.Test.Maps")
+                .Build();
+
+            _connection = new SQLiteConnection(connectionString);
+            var files = new List<string>
+            {
+                ReadScriptFile("CreateAnimalTable"),
+                ReadScriptFile("CreateFooTable"),
+                ReadScriptFile("CreateMultikeyTable"),
+                ReadScriptFile("CreatePersonTable"),
+                ReadScriptFile("CreateCarTable")
+            };
+
+            foreach (var setupFile in files)
+            {
+                _connection.Execute(setupFile);
+            }
+        }
+
+        private string ReadScriptFile(string name)
+        {
+            string fileName = GetType().Namespace + ".Sql." + name + ".sql";
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileName))
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
