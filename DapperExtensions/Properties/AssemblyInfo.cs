d0b2ea319dbfb0ff29e1efe9fc8325d492098cf1
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2017-12-28 14:55:45 -0300

    Migrated everything to NET 4.5.1, removed NET45 project and DNXCore projects which are not required anymore, bumped version to 1.6.4

--- DapperExtensions/Properties/AssemblyInfo.cs
+++ DapperExtensions/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("792b61ca-432b-4db2-a5b3-d2fb5043d0ed")]
 
-[assembly: InternalsVisibleTo("DapperExtensions.Test")]
-[assembly: InternalsVisibleTo("DapperExtensions.Test.SQLite")]
+[assembly: InternalsVisibleTo("DapperExtensions.Test,PublicKey=0024000004800000940000000602000000240000525341310004000001000100eb7f5f4e0d9b43232abc78d4560897cba0fb5c61a8a20ff58f597fc3f9b83f4117b773f139fd94d0c69bd344bd601742b6cfab02b180fc35e85c87c951063a4024f2f52c893e2036d5087fda350b8b36cef3ead5f412a48665e88d4a1db529f67199ff2bca07e39c0e3060037adb8a3b23b67f00d7daf4729c37e2962d83eee6")]
+[assembly: InternalsVisibleTo("DapperExtensions.Test.SQLite,PublicKey=0024000004800000940000000602000000240000525341310004000001000100eb7f5f4e0d9b43232abc78d4560897cba0fb5c61a8a20ff58f597fc3f9b83f4117b773f139fd94d0c69bd344bd601742b6cfab02b180fc35e85c87c951063a4024f2f52c893e2036d5087fda350b8b36cef3ead5f412a48665e88d4a1db529f67199ff2bca07e39c0e3060037adb8a3b23b67f00d7daf4729c37e2962d83eee6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -36,8 +36,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.3.0")]
-[assembly: AssemblyFileVersion("1.6.3.0")]
-[assembly: AssemblyInformationalVersion("1.6.3")]
+[assembly: AssemblyVersion("1.6.4.0")]
+[assembly: AssemblyFileVersion("1.6.4.0")]
+[assembly: AssemblyInformationalVersion("1.6.4")]
 
 [assembly: CLSCompliant(true)]
