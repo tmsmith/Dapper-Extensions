6cfd875aa81c0a09d08cccaeb483ae2e64e3cb66
Valfrid Couto <29312511+valfrid-ly@users.noreply.github.com>
2022-02-18 12:15:14 -0300

    Avoiding overriding of values

--- DapperExtensions/Predicate/FieldPredicate.cs
+++ DapperExtensions/Predicate/FieldPredicate.cs
@@ -95,11 +95,16 @@ namespace DapperExtensions.Predicate
                 return string.Format("({0} {1}IN ({2}))", columnName, Not ? "NOT " : string.Empty, GetParameterString(sqlGenerator, parameters, values));
             }
 
-            var format = (Operator == Operator.Like && Value != null && sqlGenerator.Configuration.Dialect is OracleDialect) ?
-                    "(upper({0}) {1} upper('%'||{2}||'%'))" : "({0} {1} {2})";
+            string format;
+            
             if (Operator == Operator.BitEq && Value != null)
             {
                 format = (sqlGenerator.Configuration.Dialect is OracleDialect) ? "BITAND({0}, {2}) {1} {2}" : "{0}&{2} {1} {2}";
+            }
+            else
+            {
+                format = (Operator == Operator.Like && Value != null && sqlGenerator.Configuration.Dialect is OracleDialect) ?
+                    "(upper({0}) {1} upper('%'||{2}||'%'))" : "({0} {1} {2})";
             }
 
             return string.Format(format, columnName, GetOperatorString(), GetParameterName(sqlGenerator, parameters, parameterPropertyName, parentType));
