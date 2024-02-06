47977b0407cc882193287f26260de0e1b9f60440
ricardo.matias <rmatias@grupopie.com>
2024-02-06 21:55:57 +0000

    #282 Fixed member type validation

--- DapperExtensions/DapperImplementor.cs
+++ DapperExtensions/DapperImplementor.cs
@@ -505,7 +505,7 @@ namespace DapperExtensions
 
             parameters ??= new DynamicParameters();
 
-            if (prop.MemberInfo.DeclaringType == typeof(bool) || (prop.MemberInfo.DeclaringType.IsGenericType && prop.MemberType.GetGenericTypeDefinition() == typeof(Nullable<>) && prop.MemberInfo.DeclaringType.GetGenericArguments()[0] == typeof(bool)))
+            if (prop.MemberInfo.DeclaringType == typeof(bool) || (prop.MemberInfo.DeclaringType.IsGenericType && prop.MemberType.IsGenericType && prop.MemberType.GetGenericTypeDefinition() == typeof(Nullable<>) && prop.MemberInfo.DeclaringType.GetGenericArguments()[0] == typeof(bool)))
             {
                 var value = (bool?)propValue;
                 if (!value.HasValue)
